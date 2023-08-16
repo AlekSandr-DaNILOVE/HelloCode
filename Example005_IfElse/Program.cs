2f040b7877c00165e18f69c158d9695fbfb07846
Sid6 <sid6d7@gmail.com>
2023-08-16 18:06:54 +1000

    Example 005 Condition If Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input your username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Is That You, Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
